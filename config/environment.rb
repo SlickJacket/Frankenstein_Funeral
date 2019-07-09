df3c0f2142ea5b6f34e1adc8075b82c71bcacb21
github username <github email address>
2019-07-09 14:34:46 -0400

    more

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ Bundler.require
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 ActiveRecord::Base.logger=nil
 
-require_all 'models'
+require_all 'lib'
 
 
 require_relative '../bin/methods.rb'
